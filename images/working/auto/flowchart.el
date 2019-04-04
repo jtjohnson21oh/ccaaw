9edf5e4e03872605553ffc80cc46af3f0ef70bf4
Ryan Linnabary <linnabary.24@osu.edu>
2019-04-04 15:14:53 -0400

    update

--- images/working/auto/flowchart.el
+++ images/working/auto/flowchart.el
@@ -3,6 +3,13 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("standalone" "tikz" "border=0.005in")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "standalone"
@@ -13,6 +20,8 @@
     "tikz"
     "hyperref"
     "fixltx2e"
-    "lscape"))
+    "lscape")
+   (LaTeX-add-environments
+    '("IEEEbiography" LaTeX-env-args ["argument"] 1)))
  :latex)
 
